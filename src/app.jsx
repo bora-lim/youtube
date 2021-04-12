68280a2d366f03355ba16a72705c44116dd9b780
bora-lim <yksysr@gmail.com>
2021-04-12 17:20:43 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,26 @@
-import './app.css';
+import React, { Component, useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return (
-    <h1>Hello :) </h1>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCl5wFwyx7P86-N-0MHg3eeEvSejUB68V4",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
